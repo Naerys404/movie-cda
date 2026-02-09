e1b92e8d57991694269c43f5eb650c12b00a1af6
LauraG <naerys.404@gmail.com>
2026-02-09 15:57:20 +0100

    feat : add director form

---
+++ src/Controller/DirectorController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Director;
+use App\Form\DirectorType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class DirectorController extends AbstractController
+{
+    #[Route('/director', name: 'app_director')]
+    public function index(): Response
+    {
+        return $this->render('director/index.html.twig', [
+            'controller_name' => 'DirectorController',
+        ]);
+    }
+    #[Route('/director/add', name: 'app_director_add')]
+    public function add_director(Request $request, EntityManagerInterface $em): Response
+    {
+        $dir = new Director();
+        $form = $this->createForm(DirectorType::class, $dir);
+
+        $form->handleRequest($request);
+
+         if ($form->isSubmitted() && $form->isValid()) {
+            $dir = $form->getData();
+           $em->persist($dir);
+           $em->flush();
+
+            $this->redirectToRoute('app_director_add');
+         }
+        return $this->render('director/addDirector.html.twig', [
+            'title' => 'Ajouter un réalisateur',
+            'form'=>$form
+        ]);
+    }
+}
---
+++ src/Form/DirectorType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Director;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DirectorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label'=>'Prénom',
+                'required'=>true
+            ])
+            ->add('lastname', TextType::class, [
+                'label'=>'Nom',
+                'required'=>true
+            ])
+            ->add('birthDate', DateType::class, [
+                'label'=>'Date de naissance',
+                'widget' => 'single_text',
+            ])
+            ->add('country', TextType::class ,[
+                 'label'=>'Pays'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Director::class,
+        ]);
+    }
+}
