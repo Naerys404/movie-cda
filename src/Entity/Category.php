2f26a690c88740e49681757e862281b8a1364086
mithridatem <mathieumithridate@adrar-formation.com>
2026-02-10 12:11:06 +0100

    feat JWT + Api Plateforme

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,16 +4,45 @@ namespace App\Entity;
 
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Attribute\Groups as AttributeGroups;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/category/{id}', 
+            requirements: ['id' => '\d+'],
+            normalizationContext: ['groups' => 'category:item']),
+        new GetCollection(
+            uriTemplate: '/category',
+            normalizationContext: ['groups' => 'category:list']),
+        new Post(
+            uriTemplate:'/category',
+            status:301
+        ),
+        new Delete(
+            uriTemplate:'/category/{id}',
+            requirements: ['id' => '\d+'],
+        )
+    ],
+    order: ['id' => 'ASC', 'name' => 'ASC'],
+    paginationEnabled: true
+)]
 class Category
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[AttributeGroups(["category:list", "category:item"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[AttributeGroups(["category:list", "category:item"])]
     private ?string $name = null;
 
     public function getId(): ?int
--- src/Entity/Director.php
+++ src/Entity/Director.php
@@ -6,26 +6,48 @@ use App\Repository\DirectorRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints\Length;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Attribute\Groups as AttributeGroups;
 
 #[ORM\Entity(repositoryClass: DirectorRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/director/{id}', 
+            requirements: ['id' => '\d+'],
+            normalizationContext: ['groups' => 'director:item']),
+        new GetCollection(
+            uriTemplate: '/director',
+            normalizationContext: ['groups' => 'director:list']),
+    ],
+    order: ['id' => 'ASC', 'firstname' => 'ASC', 'lastname' => 'ASC'],
+    paginationEnabled: true
+)]
 class Director
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[AttributeGroups(["director:list", "director:item"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
     #[Length(min:2, max:50,minMessage:'Le nom est trop court', maxMessage:'Le nom est trop long')]
+    #[AttributeGroups(["director:list", "director:item"])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 50)]
+    #[AttributeGroups(["director:list", "director:item"])]
     private ?string $lastname = null;
 
     #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
+    #[AttributeGroups(["director:list", "director:item"])]
     private ?\DateTimeImmutable $birthDate = null;
 
     #[ORM\Column(length: 50, nullable: true)]
+    #[AttributeGroups(["director:list", "director:item"])]
     private ?string $country = null;
 
     public function getId(): ?int
